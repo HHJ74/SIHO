367752d85eb6a66b0dac554e40cd19895b6ccfea
HHJ74 <gpgus0704@gmail.com>
2024-08-01 18:49:29 +0900

    Change base logic

---
+++ src/constants/data.jsx
@@ -0,0 +1,23 @@
+import menu1 from '../assets/menu'
+import menu2 from '../assets/menu'
+import menu3 from '../assets/menu'
+import menu4 from '../assets/menu'
+import menu5 from '../assets/menu'
+import menu6 from '../assets/menu'
+
+import sign1 from '../assets/signature'
+import sign2 from '../assets/signature'
+import sign3 from '../assets/signature'
+import sign4 from '../assets/signature'
+
+export const menu_info =[
+  // {
+  //   year:'', pic:, ingre:''  
+  // },{},{},{},{},{}
+]
+
+export const sign_info =[
+  // {
+  //   title:'', pic:, ingre:''
+  //   },{},{},{}
+]
